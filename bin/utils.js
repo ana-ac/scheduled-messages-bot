47d798b0d602b3ccfb8c73165cb08ec3aebb4a7c
AnaAC <anaarriagacoll@gmail.com>
2019-09-14 16:45:45 +0200

    Scheduled Messages Bot - installing jsdoc and docdash and inprementing in project

--- bin/utils.js
+++ bin/utils.js
@@ -1,11 +1,24 @@
-function processReplacers(ctx, info) {
+/**
+ * @module utils
+ * @requires lodash
+ */
+
+/**
+ * Process a replacers object in another object and replace the keywords
+ * @function
+ * @name processReplacers
+ * @param {object} ctx
+ * @param {object} info
+ * @return {object}
+ */
+function _processReplacers(ctx, info) {
     if (isParameterDefined('replacers', info)) {
         return info.replacers.reduce((result, key) => {
             let processedInfo = info;
-            if (findNeedle(key, info.message)) {
+            if (_findNeedle(key, info.message)) {
                 processedInfo.message = info.message.replace(
                     key,
-                    info.empty_replacer ? '' : setValueToReplace(ctx, key)
+                    info.empty_replacer ? '' : _setValueToReplace(ctx, key)
                 );
             }
             return processedInfo;
@@ -13,16 +26,39 @@ function processReplacers(ctx, info) {
     }
     return info;
 }
-
-function setValueToReplace(ctx, key) {
-    return isParameterDefined(key.toLowerCase(), ctx) ? ctx[key.toLowerCase()] : '';
+/**
+ * Decide for what value should be replaced
+ * @function
+ * @name setValueToReplace
+ * @param {object} ctx object where find the replacer
+ * @param {string|int} key key to find in object
+ * @returns {string} Value to be replaced
+ */
+function _setValueToReplace(ctx, key) {
+    return _isParameterDefined(key.toLowerCase(), ctx) ? ctx[key.toLowerCase()] : '';
 }
 
-function findNeedle(needle, haystack) {
+/**
+ * Say if the parameter is defined
+ * @function
+ * @name findNeedle
+ * @param {array|string} needle
+ * @param {string|int} haystack
+ * @return {bool}
+ */
+function _findNeedle(needle, haystack) {
     return (haystack.indexOf(needle) !== -1);
 }
 
-function isParameterDefined(needle, haystack = null) {
+/**
+ * Check if the paraemter is defined in the haystck given
+ * @function
+ * @name isParameterDefined
+ * @param {string|int} needle
+ * @param {object|null} haystack
+ * @returns {bool}
+ */
+function _isParameterDefined(needle, haystack = null) {
     let result = false;
     if (haystack === null) {
         result = (typeof needle !== 'undefined');
@@ -32,8 +68,16 @@ function isParameterDefined(needle, haystack = null) {
     return result;
 }
 
+/**
+ * Process objects with certain info to replace keys in for each message
+ * @function
+ * @name processMessages
+ * @param {object} ctx
+ * @param {array} messages
+ * @return {array}
+ */
 exports.processMessages = (ctx, messages) => messages.reduce((result, info) => {
-    const processedInfo = processReplacers(ctx, info);
+    const processedInfo = _processReplacers(ctx, info);
     result[processedInfo.id] = processedInfo.message;
     return result;
 }, {});
@@ -42,14 +86,42 @@ exports.findNeedle = (needle, haystack) => findNeedle(needle, haystack);
 
 exports.isParameterDefined = (needle, haystack) => isParameterDefined(needle, haystack);
 
+/**
+ * Transform string in snake case format to camel case
+ * @function
+ * @name snakeCaseToCamelCase
+ * @param {string} str
+ * @return {string}
+ */
 exports.snakeCaseToCamelCase = (str) => str.replace(
     /([-_][a-z])/g, (group) => group.toUpperCase()
         .replace('-', '')
         .replace('_', '')
 );
 
+/**
+ * Transform first character of string to upper case
+ * @function
+ * @name capitalize
+ * @param {string} str
+ * @return {string}
+ */
 exports.capitalize = (str) => str.charAt(0).toUpperCase() + str.slice(1);
 
+/**
+ * Transform a sentence to snake case replacinf the spaces by underscore
+ * @function
+ * @name stringToSnakeCase
+ * @param {string} str
+ * @return {string}
+ */
 exports.stringToSnakeCase = (str) => str.toLowerCase().split(' ').join('_');
 
+/**
+ * Remove the first character of a string
+ * @function
+ * @name removeFirstChar
+ * @param {string} str
+ * @return {string}
+ */
 exports.removeFirstChar = (str) => str.slice(1);
