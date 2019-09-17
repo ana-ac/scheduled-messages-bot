062ff287890b1ea729b9098d6148f8330ab3427e
AnaAC <anaarriagacoll@gmail.com>
2019-09-18 01:10:34 +0200

    test pre commit docs

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const {
 const models = require('./src/models')(conf);
 const services = require('./src/services')(models);
 const Bot = require('./src/bot')(conf, helpers());
-const TelegramBot = require('./src/telegram_bot')(
+const TelegramBot = require('./src/telegram_bot.js')(
     conf,
     helpers(),
     services,
