0ca5803d95b75681b2fe114cabde6b4264b9355a
AnaAC <anaarriagacoll@gmail.com>
2019-09-18 00:19:32 +0200

    Scheduled Messages Bot - start to restructure the project

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 // requirements
-require('./lib')();
+const conf = require('./src/conf');
+const {
+    helpers
+} = require('./src/utils')();
+const models = require('./src/models')(conf);
+const services = require('./src/services')(models);
+const Bot = require('./src/bot')(conf, helpers());
+const TelegramBot = require('./src/telegram_bot')(
+    conf,
+    helpers(),
+    services,
+    Bot
+);
+
+TelegramBot.launch();
